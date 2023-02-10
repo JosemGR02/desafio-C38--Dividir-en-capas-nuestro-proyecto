98389a9e266e20f5eae4f30f62fbbb1253e0457b
Jose gomez <100965428+JosemGR02@users.noreply.github.com>
2023-02-10 18:02:41 -0300

    primer commit

---
+++ testeo-subida-npm/testeo.spec.js
@@ -0,0 +1,24 @@
+
+
+import { suma, division, multiplicacion, resta } from './index.js';
+
+
+test('suma 1 + 2 to equal 3', () => {
+    expect(suma(1, 2)).toBe(3);
+});
+
+test('suma 1(string) + 2 to equal 3', () => {
+    expect(suma("1", 2)).toBe(3);
+});
+
+test('resta 4 - 2 to equal 2', () => {
+    expect(resta(4, 2)).toBe(2);
+});
+
+test('mutiplicacion 4 - 2 to equal 2', () => {
+    expect(multiplicacion(4, 2)).toBe(8);
+});
+
+test('division 4 - 2 to equal 2', () => {
+    expect(division(4, 2)).toBe(2);
+});
